0f362a21e6cdc048205ce6726975d950d7bdef60
Steve Huang <shuang@broadinstitute.org>
2022-01-10 22:09:31 -0500

    turn on email notification

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
 import logging
 import logging.handlers
-import sys
 import os
+import sys
+from typing import List
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, From, To, Subject, PlainTextContent, HtmlContent
 
 
 def get_configured_logger(log_level: int = logging.DEBUG,
@@ -62,3 +66,44 @@ def absolute_file_paths(directory):
     for dirpath, _ ,filenames in os.walk(directory):
         for f in filenames:
             yield os.path.abspath(os.path.join(dirpath, f))
+
+
+def send_notification(logger: logging.Logger,
+                      notification_sender_name: str,
+                      notification_receiver_names: List[str], notification_receiver_emails: List[str],
+                      email_subject: str, email_body: str,
+                      html_body: str = None):
+    """
+    Send a Single Email to a Single Recipient
+    Shameless copy from
+    https://github.com/sendgrid/sendgrid-python/blob/main/examples/helpers/mail_example.py#L9
+
+    Provide html_body at your own risk.
+    :return:
+    """
+
+    assert "SENDGRID_API_KEY" in os.environ, \
+        'environment variable SENDGRID_API_KEY is needed.'
+    assert "SENDER_EMAIL" in os.environ, \
+        'environment variable SENDER_EMAIL is needed.'
+
+    if len(notification_receiver_emails) != len(notification_receiver_names):
+        raise ValueError("Different number of recipients and recipients' emails")
+
+    sg = SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    notification_sender_email = os.environ.get('SENDER_EMAIL')
+    failed_responses = list()
+    for i in range(len(notification_receiver_emails)):
+        message = Mail(from_email=From(notification_sender_email, notification_sender_name),
+                       to_emails=To(notification_receiver_emails[i], notification_receiver_names[i]),
+                       subject=Subject(email_subject),
+                       plain_text_content=PlainTextContent(email_body),
+                       html_content=HtmlContent(html_body) if html_body else None)
+        response = sg.client.mail.send.post(request_body=message.get())
+        if 202 != response.status_code:
+            failed_responses.append(i)
+    if 0 < len(failed_responses):
+        failures = '\n'.join([notification_receiver_names[i]+':'+notification_receiver_emails[i]
+                              for i in failed_responses])
+        logger.warning(f"Failed to send message to some receivers: {failures}")
+
